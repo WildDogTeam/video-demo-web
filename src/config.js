8b970d7034ec4471519a92a2f57168e6b791d9ef
liujie3612 <liujie@wilddog.com>
2017-12-29 21:18:22 +0800

    mod

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
  * @Author: liujie
  * @Date:   2017-12-29 16:11:18
  * @Last Modified by:   liujie3612
- * @Last Modified time: 2017-12-29 20:18:23
+ * @Last Modified time: 2017-12-29 21:18:16
  */
 module.exports = {
   'wd': {
@@ -13,6 +13,6 @@ module.exports = {
     BUCKET_NAME: "",
     ACCESS_KEY: "",
     SECRET_KEY: "",
-    domain: "";
+    domain: ""
   }
 };

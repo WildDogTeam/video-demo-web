7f71998e851b6c98ab45260eead9d922ff26d157
ihoey <mail@ihoey.com>
2018-01-25 14:12:59 +0800

    修改配置

--- src/config.js
+++ src/config.js
@@ -2,17 +2,18 @@
  * @Author: liujie
  * @Date:   2017-12-29 16:11:18
  * @Last Modified by: ihoey
- * @Last Modified time: 2017-12-29 22:02:34
+ * @Last Modified time: 2018-01-25 14:11:56
  */
 module.exports = {
   'wd': {
-    syncAppid: "ihoey",
-    videoAppid: "wd5517854395tflsgc"
+    syncAppid: "",
+    videoAppid: ""
   },
   'qiniu': {
-    BUCKET_NAME: "dodetop",
-    ACCESS_KEY: "Bo7KgOB3js5PUaZkFO-45byYGEjUMANo9mDhCm8S",
-    SECRET_KEY: "r7qTZi3tN8zja8XgSCoX4O9CPddICeXRg_vQ3iPn",
-    domain: "https://oisha19l0.qnssl.com/"
+    BUCKET_NAME: "",
+    ACCESS_KEY: "",
+    SECRET_KEY: "",
+    domain: "",
+    uploaderUrl: ""
   }
 };

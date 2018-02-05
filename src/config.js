d0abdc6c52daae16eddf5a2f598591e5fc529f2b
ihoey <mail@ihoey.com>
2018-02-05 18:02:31 +0800

    merge

--- src/config.js
+++ src/config.js
@@ -2,22 +2,18 @@
  * @Author: liujie
  * @Date:   2017-12-29 16:11:18
  * @Last Modified by: ihoey
- * @Last Modified time: 2018-01-31 16:46:49
+ * @Last Modified time: 2018-02-05 18:01:55
  */
 module.exports = {
   'wd': {
-    syncAppid: 'wd0018753635ifkvjw',
-    videoAppid: "wd5248438159qbhzao"
+    syncAppid: '',
+    videoAppid: ""
   },
   'qiniu': {
-    BUCKET_NAME: "whiteboard",
-    ACCESS_KEY: "iuYyEIqScKViXRSolvrZ3ZIr-JMqka8LrWQTNvbl",
-    SECRET_KEY: "MCJmfW6pxlj78htXb6NeNcPEc5O6bvhFUKET5xQf",
-    domain: "https://whiteboard-img.wdstatic.cn",
-    uploaderUrl: 'https://convertoffice.wilddog.com/upload'
-  },
-  'cdn': {
-    assetsSubDirectory: 'web-video-demo/',
-    assetsPublicPath: 'https://js.wdstatic.cn/'
+    BUCKET_NAME: "",
+    ACCESS_KEY: "",
+    SECRET_KEY: "",
+    domain: "",
+    uploaderUrl: ''
   }
 };

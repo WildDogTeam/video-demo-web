728fcc7d941d241c1b78fdf80a649aa88f43a753
liujie3612 <liujie@wilddog.com>
2018-02-06 15:40:08 +0800

    delete config

--- src/config.js
+++ src/config.js
@@ -1,23 +1,19 @@
 /*
  * @Author: liujie
  * @Date:   2017-12-29 16:11:18
- * @Last Modified by: ihoey
- * @Last Modified time: 2018-02-06 10:53:39
+ * @Last Modified by:   liujie3612
+ * @Last Modified time: 2018-02-06 15:36:56
  */
 module.exports = {
   'wd': {
-    syncAppid: 'wd0018753635ifkvjw',
-    videoAppid: "wd5248438159qbhzao"
+    syncAppid: '',
+    videoAppid: ''
   },
   'qiniu': {
-    BUCKET_NAME: "whiteboard",
-    ACCESS_KEY: "iuYyEIqScKViXRSolvrZ3ZIr-JMqka8LrWQTNvbl",
-    SECRET_KEY: "MCJmfW6pxlj78htXb6NeNcPEc5O6bvhFUKET5xQf",
-    domain: "https://whiteboard-img.wdstatic.cn",
-    uploaderUrl: 'https://convertoffice.wilddog.com/upload'
-  },
-  'cdn': {
-    assetsSubDirectory: 'web-video-demo/',
-    assetsPublicPath: 'https://js.wdstatic.cn/'
+    BUCKET_NAME: '',
+    ACCESS_KEY: '',
+    SECRET_KEY: '',
+    domain: '',
+    uploaderUrl: ''
   }
 };
